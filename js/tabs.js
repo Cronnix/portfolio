f74edb01a41a1b0a0159b0f33c49c5c5b41bc140
Cronnix <apweine@gmail.com>
2014-05-08 21:08:37 +0200

    Tabs animation

--- js/tabs.js
+++ js/tabs.js
@@ -1,3 +1,25 @@
-  $(function() {
-    $( "#tabs" ).tabs();
-  });
+$(document).ready(function() {
+    $("#tabs").tabs({
+
+        show: function(event, ui) {
+
+            var lastOpenedPanel = $(this).data("lastOpenedPanel");
+
+            if (!$(this).data("topPositionTab")) {
+                $(this).data("topPositionTab", $(ui.panel).position().top)
+            }                
+            $(ui.panel).hide().fadeIn(500);
+            if (lastOpenedPanel) {
+                lastOpenedPanel
+                    .toggleClass("ui-tabs-hide")
+                    .css("position", "absolute")
+                    .css("top", $(this).data("topPositionTab") + "px")
+                    .fadeOut(500, function() {
+                        $(this)
+                        .css("position", "");
+                    });
+            }
+            $(this).data("lastOpenedPanel", $(ui.panel));
+        }
+    });
+});
