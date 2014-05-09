e4c951563001e40cbe5fb2446c62ac956dfb3124
Cronnix <apweine@gmail.com>
2014-05-09 12:34:22 +0200

    Mobile scroll

--- js/tabs.js
+++ js/tabs.js
@@ -28,6 +28,47 @@ $(document).ready(function() {
   })
 });
 
-$(window).scroll(function() {
-
+$(window).on('resize', function(){
+    if ($(window).width()>915) {  
+      $('.collapse').css("display", "block");
+      $('#presentation').css("height", "100%");
+      $('.collapse').css("height", "auto");
+    }
+    else if ($(window).width()<915) {
+      //$('.collapse').css("display", "block");
+      //$('#presentation').css("height", 137);
+      $('.collapse').css("height", "auto");
+      open();
+    }
 });
+
+var animate = false;
+var collapsed = false;
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  
+    if (scroll > 50 && !animate) {
+      close();
+    }
+    else if (scroll < 50){
+      open();
+    }
+});
+
+function close() {
+  if ($(window).width()<930) {
+    animate  = true;  
+    $('.collapse').stop().slideUp(400);
+      $('#presentation').stop().animate({height: "42px",}, 400, function(){animate=false; collapsed=true;});
+  }
+}
+
+function open() {
+  if ($(window).width()<930){
+    animate  = true;
+    $('.collapse').stop().slideDown(400);
+    $('#presentation').stop().animate({ height: "137px", }, 400, function(){animate=false; collapsed=false;});
+  }
+}
+
