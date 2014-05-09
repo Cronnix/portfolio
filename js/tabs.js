b2eb4bd5b434bcf49fa6da2647529538db7b7eb7
Cronnix <apweine@gmail.com>
2014-05-09 10:36:02 +0200

    fix

--- js/tabs.js
+++ js/tabs.js
@@ -29,16 +29,5 @@ $(document).ready(function() {
 });
 
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) { //use `this`, not `document`
-      $('.collapse').slideUp(400);
-      $('#presentation').animate({
-          height: "42px",
-        }, 400);
-    }
-    if ($(this).scrollTop() < 100) { //use `this`, not `document`
-      $('.collapse').slideDown(400);
-      $('#presentation').animate({
-        height: "142px",
-      }, 400);
-    }
+
 });
